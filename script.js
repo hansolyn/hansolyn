56787f1e71f13b9f2f1b0d3d1be832670b4581e8
hansolyn <solincruit@gmail.com>
2024-11-26 10:53:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const questions = [
   { question: "시끌벅적한 사무실 vs 조용한 사무실", options: ["시끌", "조용"], score: [1, -1], category: "인간관계" },
   
   // 안정 변화
-  { question: "프로젝트 진행 중반에 기가 막힌 아이디어가 떠올랐는데,<br>처음 세운 계획대로 하기 vs 실시간으로 반영하여 계획변경", options: ["계획", "변경"], score: [1, -1], category: "안정 변화" },
+  { question: "프로젝트 진행 중반에 기가 막힌 아이디어가 떠올랐다.<br><br>처음 세운 계획대로 하기 vs 실시간으로 반영하여 계획변경", options: ["계획", "변경"], score: [1, -1], category: "안정 변화" },
   { question: "인사평가 결과와 상관없이 매년 3%씩 연봉인상 vs 인사평가 결과에 따라 매년 0~10% 연봉인상", options: ["3%", "0~10%"], score: [1, -1], category: "안정 변화" },
   { question: "매주 월요일 9시에 업무보고하기 vs 이슈 있을 때마다 보고하기", options: ["정기보고", "수시보고"], score: [1, -1], category: "안정 변화" },
   
@@ -21,7 +21,7 @@ const questions = [
 
   // 개인과 조직
   { question: "인사평가 등급/등수 전사 공개 vs 비공개", options: ["공개", "비공개"], score: [1, -1], category: "개인과 조직" },
-  { question: "프로젝트가 성공적으로 마무리되어 성과급을 준다는데,<br>기여도에 따라 다르게 받기 vs 똑같이 나눠받기", options: ["기여도에 따라", "N분의 1"], score: [1, -1], category: "개인과 조직" },
+  { question: "프로젝트가 성공적으로 마무리되어 인센티브가 나온다고 한다.<br><br>기여도에 따라 다르게 받기 vs 똑같이 나눠받기", options: ["기여도에 따라", "N분의 1"], score: [1, -1], category: "개인과 조직" },
   { question: "업무능력 뛰어나지만 사회성 없는 동료 vs 업무능력 떨어지지만 사회성 좋은 동료", options: ["업무능력", "사회성"], score: [1, -1], category: "개인과 조직" },
 
   // 기타현안
